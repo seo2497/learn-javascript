ee3af13dcfe64de6730b0773fa0add6d50f94219
seo2497 <seo2497@gmail.com>
2026-09-15 10:52:13 +0900

    Feed 변수학습

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 console.log("Hello js");
 
 //변수 : 값을 저장하는 이름을 가진 공간
-const username = "후츠릿";
+// const username = "후츠릿";
 
+let username = "후츠릿";
 console.log("username:", username);
+
+username = "강민수";
+console.log("username:", username);
+
+// 데이터타입
+const num = 10; // 숫자
+console.log("🚀 ~ num:", num, ",num type:", typeof num)
+
+const text = '안녕하세요'; // 문자열
+console.log("🚀 ~ text:", text, ",text type:", typeof text)
+
+const isDarkMode = true;  // boolean
+console.log("🚀 ~ isDarkMode typeof:", isDarkMode)
+console.log("🚀 ~ isDarkMode:", isDarkMode, ", isDarkMode type:",  typeof isDarkMode)
+
+const empty = null;
+console.log("🚀 ~ empty:", empty, typeof empty)
+
+//변수선언만
+let userage;
+console.log("🚀 ~ userage:", userage)
+
